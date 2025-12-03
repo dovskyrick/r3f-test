b26ccf15c5c22314c727d387d5b73dba8ceab7c3
Ricardo Santos <05737@protonmail.com>
2025-12-03 17:13:10 +0000

    all is well just needed to move back in time .

--- grafana-plugins/3d-orbit-attitude-plugin/src/components/SatelliteVisualizer.tsx
+++ grafana-plugins/3d-orbit-attitude-plugin/src/components/SatelliteVisualizer.tsx
@@ -86,16 +86,39 @@ export const SatelliteVisualizer: React.FC<Props> = ({ options, data, timeRange,
     if (data.series.length === 1) {
       const dataFrame = data.series[0];
 
+      // DEBUG: Raw data received
+      console.log('========== SATELLITE VISUALIZER DEBUG ==========');
+      console.log('=== RAW DATA RECEIVED ===');
+      console.log('Number of series:', data.series.length);
+      console.log('DataFrame:', dataFrame);
+      console.log('Number of fields:', dataFrame.fields.length);
+      console.log('Field names:', dataFrame.fields.map(f => f.name));
+      console.log('Field types:', dataFrame.fields.map(f => f.type));
+
       if (dataFrame.fields.length !== 8) {
         throw new Error(`Invalid number of fields [${dataFrame.fields.length}] in data frame.`);
       }
 
       let timeFieldValues = coalesceToArray(dataFrame.fields[0].values);
 
+      // DEBUG: Time field extraction
+      console.log('=== TIME FIELD ===');
+      console.log('Time field raw values:', timeFieldValues);
+      console.log('First timestamp (raw):', timeFieldValues[0]);
+      console.log('Last timestamp (raw):', timeFieldValues.at(-1));
+      console.log('First as Date:', new Date(timeFieldValues[0]));
+      console.log('Last as Date:', new Date(timeFieldValues.at(-1)));
+      console.log('Number of time points:', timeFieldValues.length);
+
       const startTimestamp: number | null = timeFieldValues[0] ?? null;
       const endTimestamp: number | null = timeFieldValues.at(-1) ?? null;
 
       if (endTimestamp !== null) {
+        // DEBUG: Initial timestamp set
+        console.log('=== INITIAL TIMESTAMP SET ===');
+        console.log('Setting initial timestamp to:', JulianDate.fromDate(new Date(endTimestamp)));
+        console.log('This is the END of data range (last point)');
+        console.log('End timestamp ISO:', new Date(endTimestamp).toISOString());
         setTimestamp(JulianDate.fromDate(new Date(endTimestamp)));
       } else {
         setTimestamp(null);
@@ -110,6 +133,12 @@ export const SatelliteVisualizer: React.FC<Props> = ({ options, data, timeRange,
             }),
           ])
         );
+        // DEBUG: Availability window
+        console.log('=== AVAILABILITY WINDOW ===');
+        console.log('Start JulianDate:', JulianDate.fromDate(new Date(startTimestamp)));
+        console.log('Stop JulianDate:', JulianDate.fromDate(new Date(endTimestamp)));
+        console.log('Start ISO:', new Date(startTimestamp).toISOString());
+        console.log('Stop ISO:', new Date(endTimestamp).toISOString());
       } else {
         setSatelliteAvailability(null);
       }
@@ -121,6 +150,17 @@ export const SatelliteVisualizer: React.FC<Props> = ({ options, data, timeRange,
         const time = JulianDate.fromDate(new Date(coalesceToArray(dataFrame.fields[0].values)[i]));
 
         const DCM_ECI_ECEF = Transforms.computeFixedToIcrfMatrix(time);
+
+        // DEBUG: First position sample
+        if (i === 0) {
+          console.log('=== FIRST POSITION SAMPLE ===');
+          console.log('Time raw:', coalesceToArray(dataFrame.fields[0].values)[i]);
+          console.log('Time as JulianDate:', time);
+          console.log('Longitude:', coalesceToArray(dataFrame.fields[1].values)[i]);
+          console.log('Latitude:', coalesceToArray(dataFrame.fields[2].values)[i]);
+          console.log('Altitude:', coalesceToArray(dataFrame.fields[3].values)[i]);
+          console.log('Coordinates Type:', options.coordinatesType);
+        }
 
         let x_ECEF: Cartesian3;
         switch (options.coordinatesType) {
@@ -151,6 +191,11 @@ export const SatelliteVisualizer: React.FC<Props> = ({ options, data, timeRange,
             break;
         }
 
+        // DEBUG: First position computed
+        if (i === 0) {
+          console.log('Computed x_ECEF:', x_ECEF);
+        }
+
         const q_B_ECI = new Quaternion(
           coalesceToArray(dataFrame.fields[4].values)[i],
           coalesceToArray(dataFrame.fields[5].values)[i],
@@ -158,17 +203,40 @@ export const SatelliteVisualizer: React.FC<Props> = ({ options, data, timeRange,
           coalesceToArray(dataFrame.fields[7].values)[i]
         );
 
+        // DEBUG: First quaternion sample
+        if (i === 0) {
+          console.log('=== FIRST QUATERNION SAMPLE ===');
+          console.log('qx:', coalesceToArray(dataFrame.fields[4].values)[i]);
+          console.log('qy:', coalesceToArray(dataFrame.fields[5].values)[i]);
+          console.log('qz:', coalesceToArray(dataFrame.fields[6].values)[i]);
+          console.log('qw:', coalesceToArray(dataFrame.fields[7].values)[i]);
+          console.log('q_B_ECI:', q_B_ECI);
+          console.log('Quaternion magnitude:', Math.sqrt(q_B_ECI.x**2 + q_B_ECI.y**2 + q_B_ECI.z**2 + q_B_ECI.w**2));
+        }
+
         positionProperty.addSample(time, x_ECEF);
 
         const q_ECI_ECEF = Quaternion.fromRotationMatrix(DCM_ECI_ECEF);
         const q_ECEF_ECI = Quaternion.conjugate(q_ECI_ECEF, new Quaternion());
         const q_B_ECEF = Quaternion.multiply(q_ECEF_ECI, q_B_ECI, new Quaternion());
 
+        // DEBUG: First quaternion final
+        if (i === 0) {
+          console.log('q_B_ECEF (final):', q_B_ECEF);
+        }
+
         orientationProperty.addSample(time, q_B_ECEF);
       }
 
       setSatellitePosition(positionProperty);
       setSatelliteOrientation(orientationProperty);
+
+      // DEBUG: Final processed data
+      console.log('=== FINAL PROCESSED DATA ===');
+      console.log('Position property created:', positionProperty);
+      console.log('Orientation property created:', orientationProperty);
+      console.log('Total samples added:', dataFrame.fields[1].values.length);
+      console.log('=================================================');
     }
   }, [data, options, isLoaded]);
 
